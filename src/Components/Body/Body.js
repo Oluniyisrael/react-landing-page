dad432673a57251e3e973ab069df35dbd2f4cae7
unknown <israeloluniyi2004@gmail.com>
2022-08-08 16:03:00 +0100

    my fist commit

---
+++ src/Components/Body/Body.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+  return (
+    <div id='mar'>
+       <span id='sr'>Make remote work</span>
+       <p>Get you stream in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar</p>
+       <div id='mar2'><a id='span' href='#'>Learn more</a></div>
+    </div>
+  )
+}
+
+export default Body
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div id='flex'>
+      <div>
+      <p id='snap'>snap</p>
+      </div>
+        <nav>
+            <ul id='nav'>
+               
+                <li>Features ↓</li>
+                <li id='tar'>Company ↓</li> 
+                <li id='tar2'>Careers</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar 
